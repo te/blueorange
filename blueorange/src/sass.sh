f2a2830e269aa236cbcf69ddffcf49e31973f6b9
Te De <teted@ymail.com>
2012-08-04 05:24:31 +0100

    reverted back

--- blueorange/src/sass.sh
+++
@@ -1,2 +0,0 @@
-#nothing fancy, just did not want to type all that everytime I started working on the theme.
-sass --watch theme_left.scss:../css/theme_left.css.php theme_right.scss:../css/theme_right.css.php partials:../css/theme_right.css.php
